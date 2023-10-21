36a74245c16c23035923b1610a49f625661e2dda
sonidhruv26 <sonidhruv.2603@gmail.com>
2023-10-22 00:20:28 +0530

    commented ssl

--- connection.js
+++ connection.js
@@ -3,20 +3,20 @@ const fs = require('fs');
 require('dotenv').config();
 
 const port = process.env.PORT; // Use 4000 if PORT is not defined
-const dbHost = process.env.DB_HOST ;
-const dbUser = process.env.DB_USER ;
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST ,
-    user: process.env.DB_USER ,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
-    ssl: {ca:fs.readFileSync("D:\\Dhruv Soni\\Downloads\\DigiCertGlobalRootCA.crt.pem")}
+    // ssl: {ca:fs.readFileSync("D:\\Dhruv Soni\\Downloads\\DigiCertGlobalRootCA.crt.pem")}
 });
 
 db.connect(err => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("Connected to MySQL");
 });
 
