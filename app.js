78b2c6de6240c1b146931e3b5fbf6b28e65f192b
sonidhruv26 <sonidhruv.2603@gmail.com>
2023-11-05 03:24:01 +0530

    added search functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const axios = require('axios');
+const requestIp = require('request-ip');
 const bodyparser = require("body-parser");
 const encoder = bodyparser.urlencoded();
 const mysql = require("./connection");
@@ -9,80 +11,297 @@ const port = process.env.PORT || 80;
 app.set("view engine", "ejs");
 app.set("views", "./Views");
 app.use(express.static(__dirname + "/Public"));
+app.use(requestIp.mw());
 
 const resultsPerPage = 10;
 
-app.get("/", (req, resp) => {
+app.get("/", async (req, resp) => {
+
+  const ipAdd = req.headers['x-forwarded-for'] ||
+    req.headers['x-real-ip'] ||
+    req.clientIp;
+  
+
+  try {
+    const response = await axios.get(`http://api.ipstack.com/${ipAdd}?access_key=5c2243be17b627e61439797c8b5a832a`);
+    const locationData = response.data;
+    console.log(locationData);
+
+    const { ip, country, city, latitude, longitude, time_zone } = locationData;
+    const sql = `INSERT INTO visitor_locations (ip_address, country, city, latitude, longitude, time_zone) 
+                     VALUES (?, ?, ?, ?, ?, ?)
+                     ON DUPLICATE KEY UPDATE count = count + 1`;
+
+    mysql.query(sql, [ip, country, city, latitude, longitude, time_zone], (err, result) => {
+      if (err) {
+        console.error('Error:', err.message);
+      }
+
+      theme = "";
+      category = "";
+      org = "";
+
+      let countUpdate = "Update visitor_count SET count = count + 1";
+      let viewAll = "Select * from sih_details";
+      mysql.query(viewAll, (err, result) => {
+        if (err) throw err;
+
+        const numOfResults = result.length;
+        const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+
+        let page = req.query.page ? Number(req.query.page) : 1;
+        // console.log(page);
+        if (page > numberOfPages) {
+          resp.redirect("/?page=" + encodeURIComponent(numberOfPages));
+        } else if (page < 1) {
+          resp.redirect("/?page=" + encodeURIComponent("1"));
+        }
+
+        // Determine the SQL limit starting number
+        const startingLimit = (page - 1) * resultsPerPage;
+        // Get the relevant number of problems for this starting page
+        viewAll = `Select * from sih_details LIMIT ${startingLimit}, ${resultsPerPage}`;
+        mysql.query(viewAll, (err, result) => {
+          if (err) throw err;
+          let iterator = page - 2 > 0 ? page - 2 : 1;
+          let endingLink =
+            iterator + 4 <= numberOfPages ? iterator + 4 : numberOfPages;
+          if (endingLink < page + 2) {
+            iterator -= page + 2 - numberOfPages;
+          }
+
+          const startPage = Math.max(1, page - 2);
+          const endPage = Math.min(numberOfPages, startPage + 4);
+
+          const paginationLinks = {
+            previous: page > 1 ? `/?page=${page - 1}` : null,
+            next: page < numberOfPages ? `/?page=${page + 1}` : null,
+            pages: [],
+          };
+
+          for (let i = startPage; i <= endPage; i++) {
+            paginationLinks.pages.push({
+              page: i,
+              url: `/?page=${i}`,
+              isActive: i === page,
+            });
+          }
+
+          // For filter select-items
+          const themeQry =
+            "SELECT DISTINCT TechnologyBucket FROM sih_details ORDER BY TechnologyBucket ASC";
+          const categoryQry = "SELECT DISTINCT Category FROM sih_details";
+          const orgQry =
+            "SELECT DISTINCT ProblemCreatersOrganization FROM sih_details ORDER BY ProblemCreatersOrganization ASC";
+
+          mysql.query(themeQry, (err, themeResult) => {
+            if (err) throw err;
+            mysql.query(categoryQry, (err, categoryResult) => {
+              if (err) throw err;
+              mysql.query(orgQry, (err, orgResult) => {
+                if (err) throw err;
+                mysql.query(countUpdate, (err, countUpdate) => {
+                  if (err) throw err;
+                  mysql.query("SELECT * FROM visitor_count", (err, countResult) => {
+                    if (err) throw err;
+                    // console.log(countResult[0].count);
+                    resp.render("index", {
+                      data: result,
+                      page,
+                      iterator,
+                      endingLink,
+                      numberOfPages,
+                      themeResult,
+                      categoryResult,
+                      orgResult,
+                      theme,
+                      category,
+                      org,
+                      paginationLinks,
+                      countResult
+                    });
+                  });
+                });
+              });
+            });
+          });
+
+        });
+
+      });
+    });
+  } catch (error) {
+    console.error('Error:', error.message);
+
+    theme = "";
+    category = "";
+    org = "";
+
+    let countUpdate = "Update visitor_count SET count = count + 1";
+    let viewAll = "Select * from sih_details";
+    mysql.query(viewAll, (err, result) => {
+      if (err) throw err;
+
+      const numOfResults = result.length;
+      const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+
+      let page = req.query.page ? Number(req.query.page) : 1;
+      // console.log(page);
+      if (page > numberOfPages) {
+        resp.redirect("/?page=" + encodeURIComponent(numberOfPages));
+      } else if (page < 1) {
+        resp.redirect("/?page=" + encodeURIComponent("1"));
+      }
+
+      // Determine the SQL limit starting number
+      const startingLimit = (page - 1) * resultsPerPage;
+      // Get the relevant number of problems for this starting page
+      viewAll = `Select * from sih_details LIMIT ${startingLimit}, ${resultsPerPage}`;
+      mysql.query(viewAll, (err, result) => {
+        if (err) throw err;
+        let iterator = page - 2 > 0 ? page - 2 : 1;
+        let endingLink =
+          iterator + 4 <= numberOfPages ? iterator + 4 : numberOfPages;
+        if (endingLink < page + 2) {
+          iterator -= page + 2 - numberOfPages;
+        }
+
+        const startPage = Math.max(1, page - 2);
+        const endPage = Math.min(numberOfPages, startPage + 4);
+
+        const paginationLinks = {
+          previous: page > 1 ? `/?page=${page - 1}` : null,
+          next: page < numberOfPages ? `/?page=${page + 1}` : null,
+          pages: [],
+        };
+
+        for (let i = startPage; i <= endPage; i++) {
+          paginationLinks.pages.push({
+            page: i,
+            url: `/?page=${i}`,
+            isActive: i === page,
+          });
+        }
+
+        // For filter select-items
+        const themeQry =
+          "SELECT DISTINCT TechnologyBucket FROM sih_details ORDER BY TechnologyBucket ASC";
+        const categoryQry = "SELECT DISTINCT Category FROM sih_details";
+        const orgQry =
+          "SELECT DISTINCT ProblemCreatersOrganization FROM sih_details ORDER BY ProblemCreatersOrganization ASC";
+
+        mysql.query(themeQry, (err, themeResult) => {
+          if (err) throw err;
+          mysql.query(categoryQry, (err, categoryResult) => {
+            if (err) throw err;
+            mysql.query(orgQry, (err, orgResult) => {
+              if (err) throw err;
+              mysql.query(countUpdate, (err, countUpdate) => {
+                if (err) throw err;
+                mysql.query("SELECT * FROM visitor_count", (err, countResult) => {
+                  if (err) throw err;
+                  // console.log(countResult[0].count);
+                  resp.render("index", {
+                    data: result,
+                    page,
+                    iterator,
+                    endingLink,
+                    numberOfPages,
+                    themeResult,
+                    categoryResult,
+                    orgResult,
+                    theme,
+                    category,
+                    org,
+                    paginationLinks,
+                    countResult
+                  });
+                });
+              });
+            });
+          });
+        });
+
+      });
+
+    });
+  }
+
+});
+
+app.get('/search', (req, resp) => {
+  const searchTerm = req.query.search;
+
   theme = "";
   category = "";
   org = "";
 
-  let countUpdate = "Update visitor_count SET count = count + 1";
-  let viewAll = "Select * from sih_details";
-  mysql.query(viewAll, (err, result) => {
-    if (err) throw err;
+  const query = `SELECT * FROM sih_details WHERE ID LIKE ? OR Title LIKE ? OR TechnologyBucket LIKE ? OR Category LIKE ? OR ProblemCreatersOrganization LIKE ?`;
+  const params = Array(5).fill(`%${searchTerm}%`);
 
-    const numOfResults = result.length;
-    const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
+  mysql.query(query, params, (err, searchResults) => {
+    if (err) {
+      console.error(err);
+      resp.status(500).send('Error searching the database');
+    } else {
 
-    let page = req.query.page ? Number(req.query.page) : 1;
-    // console.log(page);
-    if (page > numberOfPages) {
-      resp.redirect("/?page=" + encodeURIComponent(numberOfPages));
-    } else if (page < 1) {
-      resp.redirect("/?page=" + encodeURIComponent("1"));
-    }
+      const numOfResults = searchResults.length;
+      const numberOfPages = Math.ceil(numOfResults / resultsPerPage);
 
-    // Determine the SQL limit starting number
-    const startingLimit = (page - 1) * resultsPerPage;
-    // Get the relevant number of problems for this starting page
-    viewAll = `Select * from sih_details LIMIT ${startingLimit}, ${resultsPerPage}`;
-    mysql.query(viewAll, (err, result) => {
-      if (err) throw err;
-      let iterator = page - 2 > 0 ? page - 2 : 1;
-      let endingLink =
-        iterator + 4 <= numberOfPages ? iterator + 4 : numberOfPages;
-      if (endingLink < page + 2) {
-        iterator -= page + 2 - numberOfPages;
+      let page = req.query.page ? Number(req.query.page) : 1;
+      // console.log(page);
+      if (page > numberOfPages) {
+        resp.redirect(`/search?search=${searchTerm}&page=${numberOfPages}`);
+      } else if (page < 1) {
+        resp.redirect(`/search?search=${searchTerm}&page=1`);
       }
 
-      const startPage = Math.max(1, page - 2);
-      const endPage = Math.min(numberOfPages, startPage + 4);
-
-      const paginationLinks = {
-        previous: page > 1 ? `/?page=${page - 1}` : null,
-        next: page < numberOfPages ? `/?page=${page + 1}` : null,
-        pages: [],
-      };
-
-      for (let i = startPage; i <= endPage; i++) {
-        paginationLinks.pages.push({
-          page: i,
-          url: `/?page=${i}`,
-          isActive: i === page,
-        });
-      }
-
-      // For filter select-items
-      const themeQry =
-        "SELECT DISTINCT TechnologyBucket FROM sih_details ORDER BY TechnologyBucket ASC";
-      const categoryQry = "SELECT DISTINCT Category FROM sih_details";
-      const orgQry =
-        "SELECT DISTINCT ProblemCreatersOrganization FROM sih_details ORDER BY ProblemCreatersOrganization ASC";
-
-      mysql.query(themeQry, (err, themeResult) => {
+      // Determine the SQL limit starting number
+      const startingLimit = (page - 1) * resultsPerPage;
+      // Get the relevant number of problems for this starting page
+      viewAll = `Select * from sih_details LIMIT ${startingLimit}, ${resultsPerPage}`;
+      mysql.query(viewAll, (err, result) => {
         if (err) throw err;
-        mysql.query(categoryQry, (err, categoryResult) => {
+        let iterator = page - 2 > 0 ? page - 2 : 1;
+        let endingLink = iterator + 4 <= numberOfPages ? iterator + 4 : numberOfPages;
+        if (endingLink - iterator < 4) {
+          iterator = Math.max(1, endingLink - 4);
+        }
+
+        const startPage = Math.max(1, iterator);
+        const endPage = Math.min(numberOfPages, endingLink);
+
+        const paginationLinks = {
+          previous: page > 1 ? `/search?search=${searchTerm}&page=${page - 1}` : null,
+          next: page < numberOfPages ? `/search?search=${searchTerm}&page=${page + 1}` : null,
+          pages: [],
+        };
+
+        for (let i = startPage; i <= endPage; i++) {
+          paginationLinks.pages.push({
+            page: i,
+            url: `/?page=${i}`,
+            isActive: i === page,
+          });
+        }
+
+        // For filter select-items
+        const themeQry =
+          "SELECT DISTINCT TechnologyBucket FROM sih_details ORDER BY TechnologyBucket ASC";
+        const categoryQry = "SELECT DISTINCT Category FROM sih_details";
+        const orgQry =
+          "SELECT DISTINCT ProblemCreatersOrganization FROM sih_details ORDER BY ProblemCreatersOrganization ASC";
+
+        mysql.query(themeQry, (err, themeResult) => {
           if (err) throw err;
-          mysql.query(orgQry, (err, orgResult) => {
+          mysql.query(categoryQry, (err, categoryResult) => {
             if (err) throw err;
-            mysql.query(countUpdate, (err, countUpdate) => {
+            mysql.query(orgQry, (err, orgResult) => {
               if (err) throw err;
               mysql.query("SELECT * FROM visitor_count", (err, countResult) => {
                 if (err) throw err;
-                // console.log(countResult[0].count);
                 resp.render("index", {
-                  data: result,
+                  data: searchResults,
                   page,
                   iterator,
                   endingLink,
@@ -101,9 +320,7 @@ app.get("/", (req, resp) => {
           });
         });
       });
-
-    });
-
+    }
   });
 });
 
@@ -191,7 +408,6 @@ app.get("/filter", (req, resp) => {
             if (err) throw err;
             mysql.query("SELECT * FROM visitor_count", (err, countResult) => {
               if (err) throw err;
-              console.log(countResult[0].count);
               resp.render("index", {
                 data: result,
                 page,
